b9817289647fedd2f56b58f6b2be9a516af4a8cc
Abdul Basit <abdul.basit@eritheialabs.io>
2022-11-22 16:31:34 +0500

    chat basic structure

--- frontend/src/pages/ChatRoom.js
+++ frontend/src/pages/ChatRoom.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Chat from '../components/chat/Chat';
 import ChatList from '../components/chat/ChatList';
 
 const ChatRoom = () => {
   return (
+    <div>
     <div className="scrollable sidebar">
       <ChatList />
     </div>
+      <Chat />
+    </div>
+      
   )
 }
 
